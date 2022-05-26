3dd6be8e1a4ac76e029f9272b522ce829f3e356b
ProjectRexa <106005919+ProjectRexa@users.noreply.github.com>
2022-05-26 14:28:23 +0530

    Create Method_2.py

---
+++ Method_2.py
@@ -0,0 +1,14 @@
+# Original Array
+
+array = [5,9,7,2,8]
+
+print('Original array : ' ,  array)
+
+# Reversing a array
+
+reversed_array = array[::-1]
+
+
+# updated array
+
+print('Reversed array : ',  reversed_array)
